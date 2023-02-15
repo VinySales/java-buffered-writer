d477f80188cff43ccb48aa3590ed164ec1e44e61
VinySales <vinysalesreboucas@hotmail.com>
2023-02-14 23:54:28 -0300

    first commit

---
+++ src/application/Main.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String[] list = new String[] {"Vinicius", "Sales", "Rebouças"};
+		String path = "c:\\temp\\in.txt";
+		
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))){
+			for (String string : list) {
+				bw.write(string);
+				bw.newLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
